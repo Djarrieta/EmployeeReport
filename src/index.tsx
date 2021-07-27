8c0aba92071716c3a121ac7cdf81344facf2c04d
Dario Arrieta <arrieta.dario@hotmail.com>
2021-07-27 16:33:18 -0500

    services tested

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { SessionProvider } from 'context/SessionContext';
-import { GlobalErrorBoundary } from 'config/errors/GlobalErrorBoundary';
 import App from './App';
 
 ReactDOM.render(
-  <GlobalErrorBoundary>
-    <SessionProvider>
-      <App />
-    </SessionProvider>
-  </GlobalErrorBoundary>,
+  <SessionProvider>
+    <App />
+  </SessionProvider>,
   document.getElementById('root'),
 );
