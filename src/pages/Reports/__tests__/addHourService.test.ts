7c73b6754b2713ac794ec1a3e15c2ae32cea65d3
Dario Arrieta <arrieta.dario@hotmail.com>
2021-07-28 10:15:30 -0500

    more tests

--- src/pages/Reports/__tests__/addHourService.test.ts
+++
@@ -1,6 +0,0 @@
-import { addHoursService } from '../services/addHoursService';
-
-test('addHoursService', async () => {
-  const response = await addHoursService({ HD: 1, HN: 1 }, '6');
-  expect(response.name).toBe('Darío');
-});
